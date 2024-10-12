4a0f702fbc2b868391662dc0b214cde790e72d20
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-10-11 23:54:09 -0500

    Modify routes of the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { CreateEventComponent } from './components/create-event/create-event.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'create-event', component: CreateEventComponent},
     { path: '**', pathMatch: "full", redirectTo: '' }
-    //Add more routes here for the other pages
+    //Add more routes here for the other pages and components
 ];
