3e2bb8d499099737d081adfb67eb90da55189ccf
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-10-11 23:53:39 -0500

    Modify register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { AbstractControlOptions, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  private createForm() {
+    //TODO: verificar que en estas validaciones se incluyan las que ya se tenian escritas en DTO respectivo  
+    this.registerForm = this.formBuilder.group({
+      dni: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.maxLength(10)]],
+      password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]],
+      confirmaPassword: ['', [Validators.required]]
+    },
+    { validators: this.passwordsMatchValidator } as AbstractControlOptions
+
+    );
+  }
+
+  public register() {
+    console.log(this.registerForm.value);
+  }
+
+  passwordsMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmaPassword = formGroup.get('confirmaPassword')?.value;
+    // Si las contraseñas no coinciden, devuelve un error, de lo contrario, null
+    return password == confirmaPassword ? null : { passwordsMismatch: true };
+    
+  }
+
+
+
 
 }
