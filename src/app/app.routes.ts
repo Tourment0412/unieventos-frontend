a62396d4df2582aba82ded62b52c0e045b747ddc
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-10-18 22:12:41 -0500

    Add carcomponent and grid and angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
+import { CardComponent } from './components/card/card.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'create-event', component: CreateEventComponent},
+    { path: 'card', component: CardComponent},
     { path: '**', pathMatch: "full", redirectTo: '' }
     //Add more routes here for the other pages and components
 ];
