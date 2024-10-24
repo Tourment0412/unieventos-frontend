f5204d39a332395205ba2f062ea832ab7d1dc1dc
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-10-24 11:36:28 -0500

    Added some components

---
+++ src/app/components/editar-perfil/editar-perfil.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { AbstractControlOptions, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-editar-perfil',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './editar-perfil.component.html',
+  styleUrl: './editar-perfil.component.css'
+})
+export class EditarPerfilComponent {
+
+  editarPerilForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  private createForm() {
+    //TODO: verificar que en estas validaciones se incluyan las que ya se tenian escritas en DTO respectivo  
+    this.editarPerilForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.maxLength(10)]],
+      address: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]],
+      confirmaPassword: ['', [Validators.required]]
+    },
+    { validators: this.passwordsMatchValidator } as AbstractControlOptions);
+  }
+
+  public editarPerfil() {
+    console.log(this.editarPerilForm.value);
+  }
+
+  passwordsMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmaPassword = formGroup.get('confirmaPassword')?.value;
+    // Si las contraseñas no coinciden, devuelve un error, de lo contrario, null
+    return password == confirmaPassword ? null : { passwordsMismatch: true };
+    
+  }
+
+
+
+}
