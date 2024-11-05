1e42bd5027a66f7a88abc29ca860e084c57b5bf4
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-11-04 19:41:28 -0500

    Update register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { AbstractControlOptions, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { FormControl } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { Validators } from '@angular/forms'
+import { AuthService } from '../../services/auth.service';
+import { CrearCuentaDTO } from '../../dto/crear-cuenta-dto';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-register',
@@ -13,8 +18,12 @@ import { Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+
+  showValidationCodeComponent = false;
+  isLoading=false;
+
   registerForm!: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router, private dataService: DataService) {
     this.createForm();
   }
 
@@ -29,13 +38,37 @@ export class RegisterComponent {
       password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]],
       confirmaPassword: ['', [Validators.required]]
     },
-    { validators: this.passwordsMatchValidator } as AbstractControlOptions
+      { validators: this.passwordsMatchValidator } as AbstractControlOptions
 
     );
   }
 
   public register() {
-    console.log(this.registerForm.value);
+    this.isLoading = true;
+    const crearCuenta = this.registerForm.value as CrearCuentaDTO;
+    this.authService.crearCuenta(crearCuenta).subscribe({
+      next: (data) => {
+        this.isLoading = false;
+        Swal.fire({
+          title: 'Cuenta creada',
+          text: 'La cuenta se ha creado correctamente',
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        })
+        console.log(this.registerForm.get('email')?.value);
+        this.dataService.setData(this.registerForm.get('email')?.value);
+        console.log(this.dataService.getData());
+        this.router.navigate(['/validar-cuenta']);
+      },
+      error: (error) => {
+        Swal.fire({
+          title: 'Error',
+          text: error.error.respuesta,
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        })
+      }
+    });
   }
 
   passwordsMatchValidator(formGroup: FormGroup) {
@@ -43,7 +76,7 @@ export class RegisterComponent {
     const confirmaPassword = formGroup.get('confirmaPassword')?.value;
     // Si las contraseñas no coinciden, devuelve un error, de lo contrario, null
     return password == confirmaPassword ? null : { passwordsMismatch: true };
-    
+
   }
 
 
