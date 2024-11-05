4d8c34d58b2656182b1b25fb169f21a47c42c5cf
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-11-04 19:42:29 -0500

    Update gestion-eventos components
    Need to do some more changes

--- src/app/components/gestion-eventos/gestion-eventos.component.ts
+++ src/app/components/gestion-eventos/gestion-eventos.component.ts
@@ -3,74 +3,90 @@ import { EventosService } from '../../services/eventos.service';
 import { RouterModule } from '@angular/router';
 import { EventoDTO } from '../../dto/eventoDTO';
 import Swal from 'sweetalert2';
+import { AdminService } from '../../services/admin.service';
 
 
 @Component({
- selector: 'app-gestion-eventos',
- standalone: true,
- imports: [RouterModule],
- templateUrl: './gestion-eventos.component.html',
- styleUrl: './gestion-eventos.component.css'
+  selector: 'app-gestion-eventos',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './gestion-eventos.component.html',
+  styleUrl: './gestion-eventos.component.css'
 })
 export class GestionEventosComponent {
 
 
- eventos: EventoDTO[];
- seleccionados: EventoDTO[];
- textoBtnEliminar: String;
+  eventos: EventoDTO[];
+  seleccionados: EventoDTO[];
+  textoBtnEliminar: String;
+  currentPage: number = 0;
+  pages: number[] = [0];
+  eventosSiguientePag: EventoDTO[];
 
- constructor(public EventosService:EventosService, ) {
-   this.eventos = EventosService.listar();
-   this.seleccionados = [];
-   this.textoBtnEliminar = ""
- }
+  eventsAvailable: boolean = true;
 
- public seleccionar(evento: EventoDTO, estado: boolean) {
-
-
-  if (estado) {
-    this.seleccionados.push(evento);
-  } else {
-    this.seleccionados.splice(this.seleccionados.indexOf(evento), 1);
+  constructor(public EventosService: EventosService, private adminService: AdminService) {
+    //this.eventos = EventosService.listar();
+    this.eventos = [];
+    this.eventosSiguientePag=[];
+    this.obtenerEventos(this.currentPage);
+    this.obtenerEventosSig(this.currentPage);
+    this.actualizarEventsAvailable();
+    console.log(this.eventosSiguientePag);
+    console.log(this.eventsAvailable);
+    
+    
+    this.seleccionados = [];
+    this.textoBtnEliminar = ""
+    
   }
 
-
-  this.actualizarMensaje();
-
-
- }
+  public seleccionar(evento: EventoDTO, estado: boolean) {
 
 
- private actualizarMensaje() {
-  const tam = this.seleccionados.length;
-
-
-  if (tam != 0) {
-    if (tam == 1) {
-      this.textoBtnEliminar = "1 elemento";
+    if (estado) {
+      this.seleccionados.push(evento);
     } else {
-      this.textoBtnEliminar = tam + " elementos";
+      this.seleccionados.splice(this.seleccionados.indexOf(evento), 1);
     }
-  } else {
-    this.textoBtnEliminar = "";
-  }
- }
 
- public confirmarEliminacion() {
-  Swal.fire({
-    title: "Estás seguro?",
-    text: "Esta acción cambiará el estado de los eventos a Inactivos.",
-    icon: "error",
-    showCancelButton: true,
-    confirmButtonText: "Confirmar",
-    cancelButtonText: "Cancelar",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      this.eliminarEventos();
-      Swal.fire("Eliminados!", "Los eventos seleccionados han sido eliminados.", "success");
+
+    this.actualizarMensaje();
+
+
+  }
+
+
+  private actualizarMensaje() {
+    const tam = this.seleccionados.length;
+
+
+    if (tam != 0) {
+      if (tam == 1) {
+        this.textoBtnEliminar = "1 elemento";
+      } else {
+        this.textoBtnEliminar = tam + " elementos";
+      }
+    } else {
+      this.textoBtnEliminar = "";
     }
-  });
-}
+  }
+
+  public confirmarEliminacion() {
+    Swal.fire({
+      title: "Estás seguro?",
+      text: "Esta acción cambiará el estado de los eventos a Inactivos.",
+      icon: "error",
+      showCancelButton: true,
+      confirmButtonText: "Confirmar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.eliminarEventos();
+        Swal.fire("Eliminados!", "Los eventos seleccionados han sido eliminados.", "success");
+      }
+    });
+  }
 
 
   public eliminarEventos() {
@@ -82,10 +98,54 @@ export class GestionEventosComponent {
     this.actualizarMensaje();
 
 
-   }
+  }
+
+  public nextPage() {
+    this.currentPage++;
+    this.pages.push(this.currentPage);
+    this.obtenerEventos(this.currentPage);
+    this.obtenerEventosSig(this.currentPage);
+    this.actualizarEventsAvailable();
+    
+
+  }
+
+  public previousPage() {
+    this.currentPage--;
+    this.obtenerEventos(this.currentPage);
+  }
+  public actualizarEventsAvailable() {
+    this.eventsAvailable = this.eventosSiguientePag.length > 0;
+  }
 
 
- }
+
+
+  //TODO aca tambien debo de la variable para manejar la paginacion correspondiente 
+  public obtenerEventos(page: number) {
+    this.adminService.listarEventosAdmin(page).subscribe({
+      next: (data) => {
+        this.eventos = data.reply
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  public obtenerEventosSig(page: number) {
+    this.adminService.listarEventosAdmin(page+1).subscribe({
+      next: (data) => {
+        this.eventosSiguientePag = data.reply
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+
+}
 
 
 
