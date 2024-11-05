454905296700acf41c437f6dd2a75704331b1eb4
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-11-04 19:41:56 -0500

    Update home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CardGridComponent } from "../card-grid/card-grid.component";
+import { PublicoService } from '../../services/publico.service';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,24 @@ import { CardGridComponent } from "../card-grid/card-grid.component";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+
+
   eventos: [] = [];
+  constructor(private publicoService: PublicoService) {
+    this.eventos = [];
+    this.obtenerEventos();
+  }
+  //This cero must be changed for a number variable to work correctly with pagination.
+  public obtenerEventos() {
+    this.publicoService.listarEventos(0).subscribe({
+      next: (data) => {
+        this.eventos = data.reply
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
+
