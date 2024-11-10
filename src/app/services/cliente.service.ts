bb3e78b4ddd290d1da39d7c3e7f5ca2d3e4b6122
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2024-11-10 18:12:25 -0500

    Carrito de compras part 1

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MensajeDTO } from '../dto/mensaje-dto';
+import { CarItemDTO } from '../dto/car-item-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +20,13 @@ export class ClienteService {
   public obtenerEvento(id: string): Observable<MensajeDTO> {
     return this.http.get<MensajeDTO>(`${this.clienteURL}/event/get/${id}`);
   }
+
+  public agregarItemCarrito(carItemDTO: CarItemDTO): Observable<MensajeDTO> {
+    return this.http.put<MensajeDTO>(`${this.clienteURL}/shoppingcar/add-item`, carItemDTO);
+  }
+
+  public obtenerItemsCarrito(id: string): Observable<MensajeDTO> {
+    return this.http.get<MensajeDTO>(`${this.clienteURL}/shoppingcar/get-items/${id}`);
+  }
+
 }
