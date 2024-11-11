fc49547a10e430dc68d39dd22838ccd5ebf23b64
JUANMANUELUQ <juanm.isazav@uqvirtual.edu.co>
2024-11-10 21:16:55 -0500

    Admin events general pagination implemented

--- src/app/components/home-admin/home-admin.component.ts
+++ src/app/components/home-admin/home-admin.component.ts
@@ -11,21 +11,43 @@ import { AdminService } from '../../services/admin.service';
   styleUrl: './home-admin.component.css'
 })
 export class HomeAdminComponent {
+  currentPage: number = 0;
   eventos: [] = [];
+  pages: number[] = [];
+  eventsAvailable: boolean = true;
   constructor(private adminService: AdminService) {
     this.eventos = [];
-    this.obtenerEventos();
+    this.obtenerEventos(0);
   }
   //This cero must be changed for a number variable to work correctly with pagination.
-  public obtenerEventos() {
-    this.adminService.listarEventosAdmin(0).subscribe({
+  public obtenerEventos(page: number) {
+    this.adminService.listarEventosAdmin(page).subscribe({
       next: (data) => {
+        console.log(data);
+        this.pages = new Array(data.reply.totalPages);
         this.eventos = data.reply.events;
-        
+        this.currentPage=page;
+        this.actualizarEventsAvailable();
       },
       error: (error) => {
         console.error(error);
       },
     });
   }
+
+  public nextPage() {
+    this.currentPage++;
+    this.obtenerEventos(this.currentPage);
+    this.actualizarEventsAvailable();
+  }
+
+  public previousPage() {
+    this.currentPage--;
+    this.obtenerEventos(this.currentPage);
+  }
+
+  public actualizarEventsAvailable() {
+    this.eventsAvailable = this.currentPage < this.pages.length-1;
+  }
+
 }
