c1ed0970823f6db2558b0d676f49846d8fc9a7c9
JUANMANUELUQ <juanm.isazav@uqvirtual.edu.co>
2024-11-10 22:12:39 -0500

    Filters of events admin implemented
    Need revision

--- src/app/components/home-admin/home-admin.component.ts
+++ src/app/components/home-admin/home-admin.component.ts
@@ -2,23 +2,37 @@ import { Component } from '@angular/core';
 import { CardGridComponent } from '../card-grid/card-grid.component';
 
 import { AdminService } from '../../services/admin.service';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {PublicoService} from '../../services/publico.service';
+import {EventFilterDTO} from '../../dto/event-filter-dto';
 
 @Component({
   selector: 'app-home-admin',
   standalone: true,
-  imports: [CardGridComponent],
+  imports: [CardGridComponent,FormsModule,ReactiveFormsModule],
   templateUrl: './home-admin.component.html',
   styleUrl: './home-admin.component.css'
 })
 export class HomeAdminComponent {
   currentPage: number = 0;
+  filterForm!: FormGroup;
   eventos: [] = [];
   pages: number[] = [];
   eventsAvailable: boolean = true;
-  constructor(private adminService: AdminService) {
+  constructor(private adminService: AdminService,private formBuilder: FormBuilder,private publicoService: PublicoService) {
     this.eventos = [];
+    this.createForm();
     this.obtenerEventos(0);
   }
+
+  createForm() {
+    this.filterForm = this.formBuilder.group({
+      name: [''],
+      city: [''],
+      evenType: [''],
+    });
+  }
+
   //This cero must be changed for a number variable to work correctly with pagination.
   public obtenerEventos(page: number) {
     this.adminService.listarEventosAdmin(page).subscribe({
@@ -28,6 +42,19 @@ export class HomeAdminComponent {
         this.eventos = data.reply.events;
         this.currentPage=page;
         this.actualizarEventsAvailable();
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  public filter() {
+    const EventFilterDTO = this.filterForm.value as EventFilterDTO;
+    EventFilterDTO.page=0;
+    this.publicoService.filtroEventos(EventFilterDTO).subscribe({
+      next: (data) => {
+        this.eventos = data.reply
       },
       error: (error) => {
         console.error(error);
