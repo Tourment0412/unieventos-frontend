627dd1c965d2201bf80102ab700e8af4db358788
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2024-11-12 23:15:04 -0500

    otra pequeña mejora grafica

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -1,22 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { AdminService } from "../../services/admin.service";
 import * as jsPDF from 'jspdf';
 import html2pdf from 'html2pdf.js';
+import { Chart, ChartConfiguration, ChartType, registerables } from 'chart.js';
+
+Chart.register(...registerables); // Registra componentes de Chart.js
 
 class LocalidadReporte {
-  nombre: string;
-  cantidadVentas: number;
-  porcentaje: number;
-  totalVendido: number;
-
-  constructor(nombre: string, cantidadVentas: number, porcentaje: number, totalVendido: number) {
-    this.nombre = nombre;
-    this.cantidadVentas = cantidadVentas;
-    this.porcentaje = porcentaje;
-    this.totalVendido = totalVendido;
-  }
+  constructor(
+    public nombre: string,
+    public cantidadVentas: number,
+    public porcentaje: number,
+    public totalVendido: number
+  ) {}
 }
 
 @Component({
@@ -24,42 +22,78 @@ class LocalidadReporte {
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './reportes.component.html',
-  styleUrls: ['./reportes.component.css'] // Corregido: styleUrls en plural
+  styleUrls: ['./reportes.component.css']
 })
-
-export class ReportesComponent {
+export class ReportesComponent implements OnInit {
   codigoEvento: string = '';
-  totalRecaudado: number = 0; // Cambiado a tipo number
-  totalCupones: number = 0; // Cambiado a tipo number
+  totalRecaudado: number = 0;
+  totalCupones: number = 0;
   localidadReportes: LocalidadReporte[] = [];
+  localidadNombres: string[] = [];
+  localidadCantidadVentas: number[] = [];
 
-  constructor(private route: ActivatedRoute, private adminService: AdminService) {
+  constructor(private route: ActivatedRoute, private adminService: AdminService) {}
+
+  ngOnInit() {
     this.route.params.subscribe((params) => {
       this.codigoEvento = params['id'];
-      this.adminService.obtenerReporte(this.codigoEvento).subscribe({
-        next: (data) => {
-          this.totalRecaudado = data.reply.totalSales;
-          this.totalCupones = data.reply.totalTickets;
-
-          const tam = data.reply.percentageSoldByLocation.length;
-          this.localidadReportes = []; // Limpia el array antes de llenarlo
-
-          for (let i = 0; i < tam; i++) {
-            this.localidadReportes.push(
-              new LocalidadReporte(
-                data.reply.percentageSoldByLocation[i].locationName,
-                data.reply.quantitySoldByLocation[i].quantitySold,
-                data.reply.percentageSoldByLocation[i].percentageSold,
-                data.reply.soldByLocation[i].totalSold
-              )
-            );
-          }
-        },
-        error: (error) => {
-          console.error(error);
-        },
-      });
+      this.cargarDatos();
     });
+  }
+
+  cargarDatos() {
+    this.adminService.obtenerReporte(this.codigoEvento).subscribe({
+      next: (data) => {
+        this.totalRecaudado = data.reply.totalSales;
+        this.totalCupones = data.reply.totalTickets;
+
+        this.localidadReportes = data.reply.percentageSoldByLocation.map((location: any, index: number) =>
+          new LocalidadReporte(
+            location.locationName,
+            data.reply.quantitySoldByLocation[index].quantitySold,
+            location.percentageSold,
+            data.reply.soldByLocation[index].totalSold
+          )
+        );
+
+        // Extrae los nombres y cantidades de ventas para el gráfico
+        this.localidadNombres = this.localidadReportes.map(item => item.nombre);
+        this.localidadCantidadVentas = this.localidadReportes.map(item => item.cantidadVentas);
+
+        // Genera el gráfico con los datos cargados
+        this.generarGrafico();
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  generarGrafico() {
+    const ctx = document.getElementById('ventasChart') as HTMLCanvasElement;
+    const config: ChartConfiguration = {
+      type: 'bar' as ChartType,
+      data: {
+        labels: this.localidadNombres,
+        datasets: [
+          {
+            label: 'Cantidad de Ventas',
+            data: this.localidadCantidadVentas,
+            backgroundColor: '#c794fa',
+            borderColor: '#65558f',
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: { beginAtZero: true }
+        }
+      }
+    };
+
+    new Chart(ctx, config);
   }
 
   generarPDF(): void {
@@ -70,7 +104,7 @@ export class ReportesComponent {
 
     const options = {
       filename: nombreArchivo,
-      jsPDF: { format: 'a4' }
+      jsPDF: { format: 'a3' }
     };
     if (element) {
       html2pdf().set(options).from(element).save();
